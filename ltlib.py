5ffce514fcbea79a730fcf54e1087755eb4fab23
Jari Saari <jari.m.saari@gmail.com>
2024-01-13 14:59:22 +0200

    Muuta longterm-piirto olioksi.

--- ltlib.py
+++ ltlib.py
@@ -1,28 +1,29 @@
 import drawlongterm
 import cairo 
 
-def draw():
+class LongTerm:
+  def __init__(self, filename):
+    self.surface = cairo.SVGSurface(filename, 740, 700)
+    self.context = cairo.Context(self.surface)
+    drawlongterm.drawBackGround(self.context)
 
-    filename = "out.svg"
-    surface = cairo.SVGSurface(filename, 740, 700)
-    context = cairo.Context(surface)
+  def addProgram(self, weekday, start, stop, title):
+    drawlongterm.addProgram(self.context, weekday, start, stop, title)
 
-    drawlongterm.drawBackGround(context)
-
-    drawlongterm.addProgram(context, 4, 20*60+00, 20*60+29, "Rahusen punatäh.")
-    drawlongterm.addProgram(context, 4, 20*60+29, 20*60+51, "Yle Uutiset")
-    drawlongterm.addProgram(context, 4, 20*60+51, 21*60+00, "Urheiluruutu")
-    drawlongterm.addProgram(context, 4, 21*60+00, 22*60+00, "Presidentinvaalit")
-    
-    drawlongterm.addProgram(context, 5, 19*60+45, 20*60+29, "Midsomer Murders")
-    drawlongterm.addProgram(context, 5, 20*60+29, 20*60+45, "Yle Uutiset")
-    drawlongterm.addProgram(context, 5, 20*60+45, 21*60+00, "Urheiluruutu")
-    drawlongterm.addProgram(context, 5, 21*60+00, 22*60+15, "Hengaillaan")
-
-    context.save()
+  def save(self):
+    self.context.save()
     #snippet.draw_func(cr, 740, 700)
-    context.restore()
-    context.show_page()
-    surface.finish()
+    self.context.restore()
+    self.context.show_page()
+    self.surface.finish()
 
-draw()
+lt = LongTerm("out.svg")
+lt.addProgram(4, 20*60+00, 20*60+29, "Rahusen punatäh.")
+lt.addProgram(4, 20*60+29, 20*60+51, "Yle Uutiset")
+lt.addProgram(4, 20*60+51, 21*60+00, "Urheiluruutu")
+lt.addProgram(4, 21*60+00, 22*60+00, "Presidentinvaalit")
+lt.addProgram(5, 19*60+45, 20*60+29, "Midsomer Murders")
+lt.addProgram(5, 20*60+29, 20*60+45, "Yle Uutiset")
+lt.addProgram(5, 20*60+45, 21*60+00, "Urheiluruutu")
+lt.addProgram(5, 21*60+00, 22*60+15, "Hengaillaan")
+lt.save()
